4454d38a4e4b3069c79f8556bc936cbb05fa59b7
qiannnly <qianlinggg@gmail.com>
2024-07-16 20:57:32 +0800

    added auth for user sign in

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+import { Form } from "../components/ui/form";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LoginSchema } from "../schema/LoginSchema";
+import { Button } from "../components/ui/button";
+import FormFields from "../components/auth/FormFields";
+import AuthCard from "../components/auth/AuthCard";
+
+type User = Yup.InferType<typeof LoginSchema>;
+
+const SignIn = () => {
+  const { signIn } = useUserContext();
+  const navigate = useNavigate();
+
+  const form = useForm<User>({
+    resolver: yupResolver(LoginSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit = async ({ email, password }: User) => {
+    try {
+      const result = await signIn(email, password);
+      if (result !== null) {
+        navigate("/dashboard");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <AuthCard
+      title="Welcome back"
+      footerLabel="Do not have an account?"
+      footerSpan="Sign Up"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-9">
+          <FormFields name="email" placeholder="Email" type="email" />
+          <FormFields name="password" placeholder="Password" type="text" />
+          <Button className="w-full" type="submit">
+            Log In
+          </Button>
+        </form>
+      </Form>
+    </AuthCard>
+  );
+};
+
+export default SignIn;
