83a939fdab318aec96fb45a7caff3f805aa062d0
qiannnly <qianlinggg@gmail.com>
2024-07-17 16:53:55 +0800

    adding top and bottom padding

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useUserContext } from "../../context/UserContext";
 import { NavBarItems } from "./NavBarItems";
 import { MenuIcon } from "lucide-react";
@@ -7,13 +7,19 @@ import { Button } from "../ui/button";
 
 const NavBar = () => {
   const { logOut, user } = useUserContext();
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <nav className="w-full top-0 fixed text-white bg-[#643A4C]">
+      <nav className="w-full pt-4 pb-2 top-0 fixed text-white bg-[#643A4C]">
         <div className="flex justify-between pb-3 px-8 items-center">
-          <div className="text-3xl w-2/6 pt-3 z-10">Bill Splitter</div>
+          <div
+            className="text-3xl w-2/6 pt-3 z-10 "
+            onClick={() => navigate("/dashboard")}
+          >
+            Bill Splitter
+          </div>
           <div
             className={`bg-[#643A4C] w-full left-0 absolute justify-between  py-5 lg:flex lg:w-4/6 lg:static ${
               open ? "top-12" : "top-[-490px]"
