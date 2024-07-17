27a6a634756f862b519d7d6228035b9e3f276b8c
qiannnly <qianlinggg@gmail.com>
2024-07-17 14:34:37 +0800

    updated files

---
+++ src/pages/Error.tsx
@@ -0,0 +1,12 @@
+const Error = () => {
+  return (
+    <>
+      <div className="m-80  text-center space-y-3">
+        <div className="font-bold">Error 404</div>
+        <div>Sorry, the page you are looking for does not exist.</div>
+      </div>
+    </>
+  );
+};
+
+export default Error;
