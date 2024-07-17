30a46f7fae5d7401845a9127024bfd75f9a36fdc
qiannnly <qianlinggg@gmail.com>
2024-07-17 14:40:42 +0800

    updated ui of error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,7 @@
 const Error = () => {
   return (
     <>
-      <div className="m-80  text-center space-y-3">
+      <div className="mx-auto mt-40 text-center space-y-3">
         <div className="font-bold">Error 404</div>
         <div>Sorry, the page you are looking for does not exist.</div>
       </div>
