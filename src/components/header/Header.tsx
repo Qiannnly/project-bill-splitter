5a0c559837b06bba2edfae34d841f8fc4360b65b
Qiannnly <qianlinggg@gmail.com>
2024-07-17 23:40:44 +0800

    updated margin of header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
 const Header = ({ header }: HeaderProps) => {
   return (
     <>
-      <div className=" flex justify-center items-center mt-40 lg:mb-10">
+      <div className=" flex justify-center items-center mt-40 lg:mb-5">
         <h2 className="font-semibold text-center text-2xl">{header}</h2>
       </div>
     </>
