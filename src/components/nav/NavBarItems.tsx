3274b1989c46ee822a8656b254f0eecba087bb9b
qiannnly <qianlinggg@gmail.com>
2024-07-17 13:59:47 +0800

    added nav bar items

---
+++ src/components/nav/NavBarItems.tsx
@@ -0,0 +1,14 @@
+export const NavBarItems = [
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+  },
+];
