b5c7cbb45899eae421dd618f4d8e7a2eabfb69be
Qiannnly <qianlinggg@gmail.com>
2024-07-17 22:42:00 +0800

    updated ui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,9 @@ const Header = ({ header }: HeaderProps) => {
   return (
     <>
       <div className="border flex justify-center items-center mt-34 mb-8 lg:mt-36 lg:mb-10">
-        <h2 className="font-semibold text-center text-2xl mt-10">{header}</h2>
+        <h2 className="font-semibold text-center text-2xl mt-28 mb-5">
+          {header}
+        </h2>
       </div>
     </>
   );
