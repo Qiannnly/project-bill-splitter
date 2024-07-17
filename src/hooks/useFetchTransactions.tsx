91448b9636e4c8a18bffea99917818751c9de9b0
qiannnly <qianlinggg@gmail.com>
2024-07-17 13:21:13 +0800

    added custom hook for fetching transactions based on activity and group ids

---
+++ src/hooks/useFetchTransactions.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { fetchTransactionList, getUser } from "../lib/utils";
+import { Transactions } from "../shared/types";
+
+const useFetchTransactions = (groupId: string, activityId: string) => {
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      setIsLoading(true);
+
+      if (groupId && activityId) {
+        const transactionList = await fetchTransactionList(activityId);
+
+        const response: Transactions[] = [];
+        transactionList.forEach((doc) => {
+          const id = doc.id;
+          const item = doc.data().item;
+          const totalAmount = doc.data().totalAmount;
+          const splitOption = doc.data().splitOption;
+          const payerId = doc.data().payerId;
+
+          getUser(groupId, payerId).then((result) => {
+            const transactionObj = {
+              id,
+              item,
+              totalAmount,
+              splitOption,
+              payer: result,
+            };
+            response.push(transactionObj);
+          });
+        });
+        setTransactions(response);
+      }
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    };
+    fetchTransactions();
+  }, [groupId, activityId]);
+  return { transactions, isLoading } as const;
+};
+
+export default useFetchTransactions;
